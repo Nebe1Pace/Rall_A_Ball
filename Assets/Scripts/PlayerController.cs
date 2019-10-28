6f866da74a7217c166575d4bd60d8ab7419ca52d
Nikita <45326590+Nebe1Pace@users.noreply.github.com>
2019-10-28 15:54:19 +0500

    git add

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public Text countText;
+	public Text winText;
+	private int count;
+
+	void Start() {
+		count = 0;
+		//countText = GetComponent<Text>();
+		SetCountText();
+		winText.text = "";
+	}
+
+	// Before physics calculations
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+		GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText();
+		}
+	}
+
+	void SetCountText() {
+		countText.text = "POINTS: " + count.ToString();
+		if (count >= 15) {
+			winText.text = "VICTORY";
+		}
+	}
+}
